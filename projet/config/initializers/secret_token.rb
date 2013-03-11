ed6667a7250566aed7cca88777e9de2ce089a8ab
rperbost <rperbost@gmail.com>
2013-03-11 14:12:49 +0100

    initialisation du projet

---
+++ projet/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projet::Application.config.secret_token = '9e1c9f2fa3e492ab217fa3507e6a0b3d7be9a6a41f78e8e37e62b7e36730b7671c5ea99d4bd0843b09c84e942286b76058b574bbb4387fb22b01589804384be0'
